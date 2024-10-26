ce590a7ad71229c1f086d3931326e0e17e413c61
bitficker <shopper.tiago@gmail.com>
2024-10-25 23:16:38 -0300

    code completed

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ print(train_images.shape)
 print(len(train_labels))
 
 print(train_labels)
+print(f"CLASS: {train_labels[1]}")
 
 print(test_images.shape)
 
@@ -31,7 +32,7 @@ print(len(test_labels))
 
 
 plt.figure()
-plt.imshow(train_images[0])
+plt.imshow(train_images[1])
 plt.colorbar()
 plt.grid(False)
 plt.show()
@@ -70,6 +71,64 @@ np.argmax(predictions[0])
 
 test_labels[0]
 
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.imshow(img, cmap=plt.cm.binary)
+    
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+    
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label], 
+                                100*np.max(predictions_array), 
+                                class_names[true_label], 
+                                color=color))
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range[10], predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+        
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1, 2, 1)
+plot_value_array(i, predictions, test_labels)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  test_labels)
+plt.show()
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+plt.show()
 
 
 
